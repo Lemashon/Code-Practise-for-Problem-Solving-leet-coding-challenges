f69edafad1a447706f70da3470134fb7e63df61f
Lemashon <lemakipeno@gmail.com>
2022-12-26 14:35:09 +0300

    additional files

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+   
+print(isValid("aabcbc"))  # Output: True
+print(isValid("aabcbc"))  # Output: True
+print(isValid("aabcbc"))  # Output: True
+print(isValid("abcabcababcc"))  # Output: True
+print(isValid("abccba"))  # Output: False
+print(isValid("cababc"))  # Output: False
